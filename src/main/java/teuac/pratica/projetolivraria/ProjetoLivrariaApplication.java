c6e78cb72daf649892b46b6940453af74d8d33ca
teuac <cauetcampos@outlook.com>
2024-05-28 17:35:01 -0300

    projeto criado

---
+++ src/main/java/teuac/pratica/projetolivraria/ProjetoLivrariaApplication.java
@@ -0,0 +1,13 @@
+package teuac.pratica.projetolivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLivrariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoLivrariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/teuac/pratica/projetolivraria/ProjetoLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package teuac.pratica.projetolivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLivrariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
